fede9b115a80938afd5e24f8bcf578d3e9acab72
soorMSWE <soor@uci.edu>
2024-05-17 12:33:08 -0700

    implemented two vulnerabilities
    two vulnerabilities have been implemented. i will do the others some later time

--- bankpy/bank.py
+++ bankpy/bank.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
-from database import AccountBalance, db
+from database import BankAccount, db
 import re
 
 def create_app():
@@ -31,7 +31,7 @@ def create_app():
     @app.route('/<username>/dashboard', methods=["GET", "POST"])
     def dashboard(username):
         print("In the dashboard - username: " + username)
-        balance = AccountBalance.query.filter_by(username=username).first().balance
+        balance = BankAccount.query.filter_by(username=username).first().balance
         print("In the dashboard - balance: " + str(balance))
         return render_template('dashboard.html', username=username, balance=balance) 
 
@@ -41,9 +41,9 @@ def create_app():
         password = request.form.get("password")
         print("Username - " + username + "; Password - " + password)
         if not username or not password:
-            return '<h3>Invalid Input or Invalid Account ID or Invalid Password!</h3>'
+            return '<h3>Invalid Input or Invalid Password!</h3>'
         else:
-            user = AccountBalance.query.filter_by(username=username).first()
+            user = BankAccount.query.filter_by(username=username).first()
             if user and user.check_password(password):
                 return redirect(url_for('dashboard', username=username)) 
             else:
@@ -61,10 +61,11 @@ def create_app():
         if re.search(command_pattern, username) or re.fullmatch(ip_pattern, username) or \
            not username or not password or not password2 or password != password2:
             return '<h3>Invalid Input or Invalid Account ID or Invalid Password!</h3>'
-
+        if db.session.query(BankAccount.username).first():
+            print("USERNAME TAKEN")
         try:
             print(f"[Register Request] Username - {username}; Password - {password}")
-            new_account = AccountBalance(username=username, password=password, balance=19.99)
+            new_account = BankAccount(username=username, password=password, balance=19.99)
             db.session.add(new_account)
             db.session.commit()
             print(f"[Request Success]")
@@ -74,7 +75,7 @@ def create_app():
             db.session.rollback()
             print(f"Error occurred: {e}")
             if str(e).find("UNIQUE constraint failed"):
-                return '<h3>Username Exist! Please login or register with another name!</h3>', 400
+                return '<h3>Username Taken! Please login or register with a different username!</h3>', 400
             else:
                 return '<h3>Invalid Input or Invalid Account ID or Invalid Password!</h3>', 400
 
@@ -86,43 +87,35 @@ def create_app():
 
     @app.route('/<username>/dashboard/deposit', methods=["GET", "POST"])
     def deposit(username):
-        balance = AccountBalance.query.filter_by(username=username).first().balance
+        balance = BankAccount.query.filter_by(username=username).first().balance
         return render_template('deposit.html', username=username, balance=balance) 
 
     @app.route('/<username>/dashboard/withdraw', methods=["GET", "POST"])
     def withdraw(username):
-        balance = AccountBalance.query.filter_by(username=username).first().balance
+        balance = BankAccount.query.filter_by(username=username).first().balance
         return render_template('withdraw.html', username=username, balance=balance) 
     
     @app.route("/withdraw_verify/<username>", methods=["GET", "POST"])
     def withdraw_verify(username):
         withdraw_amount = int(request.form.get("withdraw_amount"))
-        input_username = str(request.form.get("username"))
-        print(withdraw_amount, input_username)
-        if input_username != username or withdraw_amount <= 0.0:
-            return '<h3>Invalid Input!</h3>', 400
-        user = AccountBalance.query.filter_by(username=username).first()
+        user = BankAccount.query.filter_by(username=username).first()
         if user:
             if withdraw_amount > user.balance:
                 return '<h3>The input amount is greater than your balance!</h3>', 400
             user.update_balance(-withdraw_amount)
             db.session.commit()
-        print("Updated Balance: ", str(AccountBalance.query.filter_by(username=username).first().balance))
+        print("Updated Balance: ", str(BankAccount.query.filter_by(username=username).first().balance))
         return redirect(url_for('dashboard', username=username)) 
 
 
     @app.route("/deposit_verify/<username>", methods=["GET", "POST"])
     def deposit_verify(username):
         deposit_amount = int(request.form.get("deposit_amount"))
-        input_username = str(request.form.get("username"))
-        print(deposit_amount, input_username)
-        if input_username != username or deposit_amount <= 0.0:
-            return '<h3>Invalid Input!</h3>', 400
-        user = AccountBalance.query.filter_by(username=username).first()
+        user = BankAccount.query.filter_by(username=username).first()
         if user:
             user.update_balance(+deposit_amount)
             db.session.commit()
-        print("Updated Balance: ", str(AccountBalance.query.filter_by(username=username).first().balance))
+        print("Updated Balance: ", str(BankAccount.query.filter_by(username=username).first().balance))
         return redirect(url_for('dashboard', username=username)) 
 
 
--- bankpy/database.py
+++ bankpy/database.py
@@ -6,8 +6,8 @@ getcontext().prec = 10
 
 db = SQLAlchemy()
 
-class AccountBalance(db.Model):
-    __tablename__ = 'account_balance'
+class BankAccount(db.Model):
+    __tablename__ = 'bank_accounts'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
