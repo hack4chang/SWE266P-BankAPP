4e9c4cd34ae20596c9dd27e826449beba8b467df
dloe <dylanloe@gmail.com>
2024-05-16 19:06:25 -0700

    Somehow this got removed in the merge, readded back

--- bankpy/bank.py
+++ bankpy/bank.py
@@ -13,6 +13,16 @@ def create_app():
     with app.app_context():
         db.create_all()
 
+    """
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html'), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(e):
+        return render_template('500.html'), 500
+    """
+
     @app.route('/')
     def home():
         # print(session.get('id'))
